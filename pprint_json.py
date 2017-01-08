d9b50804dbc3ffbab588948f5543b6864c9676d5
Denis Cherednikov <denis.cherednikov@gmail.com>
2017-01-08 13:11:23 +0300

    Pretty Print JSON

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,22 @@
 import json
-
+import os
 
 def load_data(filepath):
-    pass
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath,'r', encoding='utf-8') as json_file:
+        return json.load(json_file)
 
 
 def pretty_print_json(data):
-    pass
+	return json.dumps(data, indent=4,ensure_ascii=False)
 
 
 if __name__ == '__main__':
-    pass
+    filepath = input("Введите путь к файлу: ")
+    shops = load_data(filepath)
+    
+    if shops is None:
+        print("Файла или папки с таким названием не существует.")
+    else:
+        print(pretty_print_json(shops))
